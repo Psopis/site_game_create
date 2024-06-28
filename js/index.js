a7f5996fed7efa2a193c257225f03da67056c03d
Psopis <konevvik05@mail.ru>
2024-06-29 04:31:25 +0700

    asdasdasdвввmm

--- js/index.js
+++ js/index.js
@@ -146,13 +146,8 @@ class Player {
 
 
 let player = new Player();
-let tg = window.Telegram.WebApp;
-var WebApp = window.Telegram.WebApp;
 
+var WebApp = window.Telegram.WebApp;
 WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
-var initData = Telegram.WebApp.initData || '';
-var initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
-alert(tg.initDataUnsafe.user)
-alert(tg)
-alert(window.Telegram.WebApp.sendData(playerScore))
+
 player.startGame();
